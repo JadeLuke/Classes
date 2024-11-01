66b2fa1d913c54e6a9f75815818c555257569094
JadeLuke <jadeluke.booysen@gmail.com>
2024-11-01 09:19:12 +0200

    typescript practice

--- index.js
+++ index.js
@@ -1,22 +1,74 @@
-var Person = /** @class */ (function () {
-    function Person(name, age, stipdend, gender) {
-        this.name = name;
-        this.age = age;
-        this.stipend = stipdend;
-        this.gender = gender;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Vehicle = /** @class */ (function () {
+    function Vehicle(wheels) {
+        this.wheels = wheels;
     }
-    Person.prototype.introduction = function () {
-        return "Hello I am ".concat(this.name, " and I am a ").concat(this.gender, ".");
+    Vehicle.prototype.move = function () {
+        return "This vehicle is moving";
     };
-    Person.prototype.personAge = function () {
-        return "I am ".concat(this.age, " years old");
-    };
-    Person.prototype.income = function () {
-        return "I earn R".concat(this.stipend, " a month");
-    };
-    return Person;
+    return Vehicle;
 }());
-var personOne = new Person('Jazi', 24, 6000, 'male');
-console.log(personOne.introduction());
-console.log(personOne.personAge());
-console.log(personOne.income());
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car(doors, wheels) {
+        var _this = _super.call(this, wheels) || this;
+        _this.doors = doors;
+        return _this;
+    }
+    Car.prototype.start = function () {
+        return "engine is on";
+    };
+    return Car;
+}(Vehicle));
+var Bicycle = /** @class */ (function (_super) {
+    __extends(Bicycle, _super);
+    function Bicycle(isPedal, wheels) {
+        var _this = _super.call(this, wheels) || this;
+        _this.isPedal = isPedal;
+        return _this;
+    }
+    Bicycle.prototype.pedal = function () {
+        if (this.isPedal) {
+            console.log("Has pedals");
+        }
+    };
+    return Bicycle;
+}(Vehicle));
+var bicycle = new Bicycle(true, 2);
+console.log(bicycle.pedal());
+var Employees = /** @class */ (function () {
+    function Employees(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+    Employees.prototype.work = function () {
+        return "Employee name is ".concat(this.name);
+    };
+    return Employees;
+}());
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(name, id, salary) {
+        var _this = _super.call(this, name, id) || this;
+        _this.salary = salary;
+        return _this;
+    }
+    Manager.prototype.pay = function () {
+        return "Salary is ".concat(this.salary);
+    };
+    return Manager;
+}(Employees));
--- index.ts
+++ index.ts
@@ -1,67 +1,125 @@
-class Vehicle {
-wheels: number
+// class User {
+//     name: string
+//     age: number
 
-constructor(wheels:number){
-    this.wheels = wheels
-}
+// constructor(name: string, age: number){
+//     this.name = name
+//     this.age = age
+// }
 
-move(){
-    return `This vehicle is moving`
-}
+// isAdult(): boolean {
+// if(this.age >= 18){
+//     return true;
+// }
+// else{
+//     return false
+// }
+// }
+// }
 
+// const user1 = new User('jazi', 2);
+// console.log(user1.isAdult());
 
-}
+// class Banking {
+//     deposit: number;
+//     accountNum = 12345
+//     withdrawal: number; 
+//     amount: number;
+//     balance: number; 
+//     newBalanace: number
+//     newDeposit: number;
+    
+//     constructor(deposit : number, withdrawal: number, newDeposit: number){
+//         this.deposit = deposit
+//         this.withdrawal = withdrawal
+//         this.amount = this.deposit
+//         this.balance = deposit - withdrawal
+//         this.newDeposit = newDeposit
+//         this.newBalanace = this.balance + newDeposit
+//     }
 
-class Car extends Vehicle {
-doors: number
+//     depositAmt(){
+//      return `you deposited R${this.deposit} your total amount is R${this.amount}`
 
-constructor(doors: number, wheels: number){
-     super(wheels)
-     this.doors = doors
-}
+//     }
 
-start(){
-    return `engine is on`
-}
+//     withdrawalAmt(){
+//         return `you have withdrawn R${this.withdrawal} your balance is R${this.balance}`
+//     }
 
+//     newBal(){
+//         return `you have deposited R${this.newDeposit} your new balance is R${this.newBalanace}`
+//     }
+// }
 
-}
+// let deposits = new Banking(500, 300, 700)
+// console.log(deposits.depositAmt());
+// console.log(deposits.withdrawalAmt());
+// console.log(deposits.newBal());
 
-class Bicycle extends Vehicle {
-    isPedal: boolean
+// class BankingAcc{
+//     accNum : string;
+//     balance: number;
 
-    constructor(isPedal: boolean, wheels: number){
-        super(wheels)
-        this.isPedal = isPedal
+//     constructor(accNum: string, initialBal: number){
+//      this.accNum = accNum;
+//      this.balance = initialBal;
+//     }
+
+//     deposit(amount: number):void {
+//         this.balance += amount;
+//          console.log(`deposit was successfull: new balance is R${this.balance}`);
+//     }
+
+//     withdraw(amount: number): void {
+//         if (this.balance >= amount){
+//             this.balance -= amount
+//             console.log(`withdrawal was successfull: new balance is R${this.balance}`);
+//         } else{
+//             console   .log(`insufficient funds`);
+//         }
+//     }
+// }
+// const account = new BankingAcc('122443', 500)
+// account.deposit(400)
+// account.withdraw(250)
+// account.withdraw(1000)
+
+class Products {
+    name: string
+    price: number
+    stock : number;
+    
+
+    constructor(name: string, price: number, stock: number){
+        this.name = name
+        this.price = price
+        this.stock = stock
     }
 
-    pedal(){
-        if(this.isPedal){
-            console.log( `Has pedals`)
+    isAvailable(): boolean {
+      if(this.stock > 0){
+        // console.log(`Out of stock`);
+        return true
+      }
+      else{
+        // console.log(`you can purchase`);
+        return false
+      }
+    }
+
+    buy(quantity: number): string{
+        if (this.stock >= quantity){
+          this.stock -= quantity
+          return ` Purchase succesfull! ${quantity} ${this.name}(s) bought. Remaining stock: ${this.stock} `            
+        }
+        else{
+            return `Out of stock`
         }
     }
-
 }
 
-let bicycle = new Bicycle(true, 2)
-console.log(bicycle.pedal());
-
-
-class Employees {
-name: string
-id: number
-constructor(name: string, id: number){
-this.name = name
-this.id = id
-}
-
-work(){
-    return `Employee name is ${this.name}`
-}
-
-}
-
-class Manager extends Employees{
-
-    
-}
+const purchase = new Products('Apple', 5, 15)
+console.log(purchase.isAvailable());
+console.log(purchase.buy(4));
+console.log(purchase.buy(4));
